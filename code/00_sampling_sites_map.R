88abec553b64570494c938e67a833b3256e37b13
fjoyce <fhjoyce@ucsb.edu>
2026-01-06 12:48:57 -0800

    delete obsolete shapefile

--- code/00_sampling_sites_map.R
+++ code/00_sampling_sites_map.R
@@ -1,6 +1,18 @@
+library(dplyr)
 library(sf)
 library(mapview)
 
 sampling_sites <- st_read("data/sampling_site_shapefile/Aquatic_Invert_Sampling_Sites.shp")
 
+#display interactive map
 mapview(sampling_sites)
+
+#extract coordinates from sf object
+coords_matrix <- st_coordinates(sampling_sites)
+
+
+#bind columns
+sampling_sites_coordinates <- bind_cols(sampling_sites, lon = coords_matrix[,1], lat = coords_matrix[,2])
+
+#not sure what the units are... aren't decimal degrees
+
