134cf22733dcb73f57806f59712ebc6c359e485b
fjoyce <fhjoyce@ucsb.edu>
2026-01-20 11:36:08 -0800

    comment out view() calls

--- code/Seawards_Jon/jon_code/01_setup.R
+++ code/Seawards_Jon/jon_code/01_setup.R
@@ -19,7 +19,8 @@ Aquatic_Sampling_Data_2025_09_11_clean <- read_excel("data/Aquatic_Sampling_Data
 View(Aquatic_Sampling_Data_2025_09_11_clean)
 
 invert_data <-read_excel(path = "data/Aquatic_Sampling_Data_2025-09-11_clean.xlsx", sheet = "Aquatic Insects" )
-view(invert_data)
+
+#view(invert_data)
 
 # clean taxa data counts (remove NA's)
 
@@ -32,7 +33,7 @@ mud_col <- "NZ Mudsnail"
 
 invert_data_nzm <- invert_data_clean %>%
   filter(.data[[mud_col]] > 0)
-view(invert_data_nzm)
+#view(invert_data_nzm)
 
 # NZM Only - Omit non NZM Taxa data
 
@@ -41,20 +42,21 @@ mud_col <- "NZ Mudsnail"
 
 nzm_only <- invert_data_clean %>%
   select(all_of(meta_cols), all_of(mud_col))
-view(nzm_only)
+#view(nzm_only)
 
 # NZM Only Present - Omits all NZM <0 data
 
 nzm_only_present <- invert_data_clean %>%
   select(all_of(meta_cols), all_of(mud_col)) %>%
   filter(.data[[mud_col]] > 0)
-view(nzm_only_present)
+#view(nzm_only_present)
 
 
 #Import Water quality data and integreate with filtered datasets
 
 water_quality <-read_excel(path = "data/Aquatic_Sampling_Data_2025-09-11_clean.xlsx", sheet = "Water Quality" )
-view(water_quality)
+#view(water_quality)
+water_quality
 
 #FIXME ^^ - start and end times + dates + sites are not consistently formatted, depth measurement should be factored to standard?, lots of N/A.. 
 
