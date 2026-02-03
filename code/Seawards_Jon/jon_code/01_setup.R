f48ba95914d66049db6e7d08ff44a2f100377285
“jseawards” <jon@seawards.us>
2026-02-03 10:54:21 -0800

    streamlined object naming, adressed FJH suggestions

--- code/Seawards_Jon/jon_code/01_setup.R
+++ code/Seawards_Jon/jon_code/01_setup.R
@@ -13,54 +13,20 @@ library(janitor)
 library(readr)
 
 # Read data (static excel file snapshot)
-
-#TODO FHJ- delete this if you don't need it
-
-Aquatic_Sampling_Data_2025_09_11_clean <- read_excel("data/Aquatic_Sampling_Data_2025-09-11_clean.xlsx")
-
-
-#View(Aquatic_Sampling_Data_2025_09_11_clean)
-
-invert_data <-read_excel(path = "data/Aquatic_Sampling_Data_2025-09-11_clean.xlsx", sheet = "Aquatic Insects" ) 
-
-#view(invert_data)
+invert_data <-read_excel(path = "data/Aquatic_Sampling_Data_2026-01-21.xlsx", sheet = "Aquatic Insects" ) 
 
 # clean taxa data counts (remove NA's)
-
 invert_data_clean <- invert_data %>%
   clean_names() %>% 
   mutate(across(where(is.numeric), ~ replace_na(.x, 0)))
 
-# NZM - filter events for mudsnail occurance
-
-# can delete this since we ran clean_names()
-#mud_col <- "NZ Mudsnail"
-
+# FILTER:events for nzm occurance while keeping other taxa
 invert_data_nzm <- invert_data_clean %>%
   filter(nz_mudsnail > 0)
-#view(invert_data_nzm)
 
-# NZM Only - Omit non NZM Taxa data
-
-#FHJ note: this is metadata columns for the full data set, 
-#create a new object if you want metadata columns for only nzms samples
-meta_cols <- invert_data_clean %>% 
-  select(date_on_vial, site, sample_type)
-  
-
-#FIXME- revisit whether this is necessary/efficient  
-#mud_col <- "NZ Mudsnail"
-nzm_only <- invert_data_clean %>%
-  select(all_of(meta_cols), all_of(mud_col))
-#view(nzm_only)
-
-# NZM Only Present - Omits all NZM <0 data
-
-nzm_only_present <- invert_data_clean %>%
-  select(all_of(meta_cols), all_of(mud_col)) %>%
-  filter(.data[[mud_col]] > 0)
-#view(nzm_only_present)
-
+# SEPERATE: only events with nzm occurance and omit other taxa 
+invert_data_nzm_separate <- invert_data_nzm %>%
+  select(date_on_vial, site, sample_type, nz_mudsnail)
 
 #Import Water quality data and integreate with filtered datasets
 
