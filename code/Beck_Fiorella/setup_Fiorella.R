3acc63a05ea8d3cfc90543b526b3af265d0f3805
fjoyce <fhjoyce@ucsb.edu>
2026-02-04 10:13:14 -0800

    update Phelps subset script, demo graph for temperature

--- code/Beck_Fiorella/setup_Fiorella.R
+++ code/Beck_Fiorella/setup_Fiorella.R
@@ -16,17 +16,42 @@
 #run the general setup script
 source("code/00_setup.R")
 
-#note: Francis will update 00_setup.R to also import water quality data
-
+#list of Phelps Creek sampling sites
 phelps_site <-c("NPB","NPB1","NPB2")
+
+#list of dipnet (sweepnet) sample types
 sample_SW<- c("SW250","SW500")
 
+#filter water quality data to just phelps sites
 water_quality_NPB<-water_quality %>%
   filter(site %in% phelps_site)
 
+#filter invert data to just Phelps sites and dipnet samples
 invert_data_NPB<-invert_data%>%
   filter(site %in% phelps_site)%>%
   filter(sample_type %in% sample_SW)
 
-Next steps:
-  Filter taxa to bioassement categories, pivot_longer(),format list of indicator taxa
+
+
+
+#Next steps:
+#Filter taxa to bioassement categories, pivot_longer(),format list of indicator taxa
+
+# demo figure for temperature
+fig_phelps_temp <- ggplot(data = water_quality_NPB, aes(x = date, y = temperature_c, group = site, color = site)) +
+  geom_point() +
+  geom_line() +
+  xlab("Date") +
+  ylab("Temperature (C)") +
+  theme_cowplot() +
+  facet_wrap(vars(site), nrow = 3)
+  
+fig_phelps_temp
+
+
+ggsave(filename = "figures/Beck_Fiorella/Phelps_Creek_temp_DRAFT.pdf",
+       plot= fig_phelps_temp,
+       bg = "white")
+
+
+
