537c474ff2f5dfbbcecf600b3f20d024fc83e06f
fjoyce <fhjoyce@ucsb.edu>
2026-02-11 09:50:18 -0800

    code review!

--- code/Beck_Fiorella/setup_Fiorella.R
+++ code/Beck_Fiorella/setup_Fiorella.R
@@ -1,4 +1,4 @@
-# =============================================================================
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Name:           setup_Fiorella.R
 # Description:    Filter code to subset of needed data
 # Author(s):      Fiorella Beck
@@ -9,11 +9,11 @@
 # 
 # Notes:          
 #                 
-# =============================================================================
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
-#run the general setup script
+# run the general setup script ----
 source("code/00_setup.R")
 
 #list of Phelps Creek sampling sites
@@ -29,15 +29,18 @@ water_quality_NPB<-water_quality %>%
 #filter invert data to just Phelps sites and dipnet samples
 invert_data_NPB<-invert_data%>%
   filter(site %in% phelps_site)%>%
-  filter(sample_type %in% sample_SW)
+  filter(sample_type %in% sample_SW) %>% 
+  mutate(dissolved_oxygen_mg_l = as.numeric(dissolved_oxygen_mg_l),
+         conductivity_specific_m_s_cm = as.numeric(conductivity_specific_m_s_cm))
+  
 
-
+# plot water quality over time by site ----
 
 
 #Next steps:
 #Filter taxa to bioassement categories, pivot_longer(),format list of indicator taxa
 
-# demo figure for temperature
+## temp -----
 
 fig_phelps_temp <- ggplot(data = water_quality_NPB, aes(x = date, y = temperature_c, group = site, color = site)) +
   geom_point() +
@@ -45,15 +48,13 @@ fig_phelps_temp <- ggplot(data = water_quality_NPB, aes(x = date, y = temperatur
   xlab("Date") +
   ylab("Temperature (C)") +
   theme_cowplot() +
-  facet_wrap(vars(site), nrow = 3) +
-  ggtitle("Temperature (C) of Phelps Creek by Site")
+  #facet_wrap(vars(site), nrow = 3) +
+  ggtitle("Temperature (C) of Phelps Creek by Site") +
+  scale_x_datetime(date_breaks = "1 year", date_labels = "%Y")
   
 fig_phelps_temp
 
-
-
-water_quality_NPB$dissolved_oxygen_mg_l<- as.numeric(as.character(water_quality_NPB$dissolved_oxygen_mg_l))
-
+## DO -----
 
 fig_phelps_DO <- ggplot(data = water_quality_NPB, aes(x = date, y = dissolved_oxygen_mg_l, group = site, color = site)) +
   geom_point() +
@@ -67,6 +68,8 @@ fig_phelps_DO <- ggplot(data = water_quality_NPB, aes(x = date, y = dissolved_ox
 
 fig_phelps_DO
 
+## pH ----
+
 fig_phelps_pH <- ggplot(data = water_quality_NPB, aes(x = date, y = p_h, group = site, color = site)) +
   geom_point() +
   geom_line() +
@@ -78,8 +81,9 @@ fig_phelps_pH <- ggplot(data = water_quality_NPB, aes(x = date, y = p_h, group =
 
 fig_phelps_pH
 
-water_quality_NPB$conductivity_specific_m_s_cm<- as.numeric(as.character(water_quality_NPB$conductivity_specific_m_s_cm))
+## conductivity ----
 
+#TODO- make conductivity units microSiemens/cm, not milliSiemens
 fig_phelps_cond <- ggplot(data = water_quality_NPB, aes(x = date, y = conductivity_specific_m_s_cm, group = site, color = site)) +
   geom_point() +
   geom_line() +
@@ -87,12 +91,11 @@ fig_phelps_cond <- ggplot(data = water_quality_NPB, aes(x = date, y = conductivi
   ylab("Specific Conductivity (mS/cm)") +
   theme_cowplot() +
   facet_wrap(vars(site), nrow = 3, scales = "free_y")+
-  ggtitle("Specific Conductivity (mS/cm) levels of Phelps Creek by Site") +
-  theme(axis.text.y = element_text(size = 5))
+  ggtitle("Specific Conductivity (mS/cm) levels of Phelps Creek by Site")
 
 fig_phelps_cond
 
-water_quality_NPB$salinity_ppt<- as.numeric(as.character(water_quality_NPB$salinity_ppt))
+## salinity ----
 
 fig_phelps_salt <- ggplot(data = water_quality_NPB, aes(x = date, y = salinity_ppt, group = site, color = site)) +
   geom_point() +
@@ -101,17 +104,16 @@ fig_phelps_salt <- ggplot(data = water_quality_NPB, aes(x = date, y = salinity_p
   ylab("Salinity (ppt)") +
   theme_cowplot() +
   facet_wrap(vars(site), nrow = 3, scales = "free_y")+
-  ggtitle("Salinity (ppt) levels of Phelps Creek by Site") +
-  theme(axis.text.y = element_text(size = 5))
+  ggtitle("Salinity (ppt) levels of Phelps Creek by Site") 
 
 fig_phelps_salt
 
 
   
-
-ggsave(filename = "figures/Beck_Fiorella/Phelps_Creek_temp_DRAFT.pdf",
-       plot= fig_phelps_temp,
-       bg = "white")
+# example code for writing to file as pdf
+#ggsave(filename = "figures/Beck_Fiorella/Phelps_Creek_temp_DRAFT.pdf",
+       #plot= fig_phelps_temp,
+       #bg = "white")
 
 
 
