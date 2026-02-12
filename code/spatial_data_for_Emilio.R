4e62164abba84302d25b32bb0e5caf2627edf3ea
fjoyce <fhjoyce@ucsb.edu>
2026-02-11 16:48:27 -0800

    add script and data: making aquatic invert data spatial

---
+++ code/spatial_data_for_Emilio.R
@@ -0,0 +1,17 @@
+source("code/00_setup.R")
+source("code/00_sampling_sites_map.R")
+
+
+spatial_invert_data <- invert_data %>% 
+  inner_join(sampling_sites, by = join_by(site == Location_Name)) %>% 
+  filter(site != "PIER") %>% 
+  filter(site != "MO1") 
+
+#mapview(spatial_invert_data, map.types = "Esri.WorldImagery")
+
+#write to file
+write_csv(x = spatial_invert_data, file = "data/for_Emilio/invert_data_spatial.csv")
+
+
+
+
