e5518e425aa78817e9680513f95306b5508ec776
beojemann <beojemann@ucsb.edu>
2026-04-16 16:11:06 -0700

    Filter bird data by zones and summarized counts

--- code/Ojemann_Emilio/01_read_filter_bird_data.R
+++ code/Ojemann_Emilio/01_read_filter_bird_data.R
@@ -1,8 +1,8 @@
 #note: this script relies on objects loaded by get_zones_FJ.R
 
-# goal: read in bird data from csv, then filter based on zone polygons
+# GOAL: read in bird data from csv, then filter based on zone polygons
 
-#read in ncos bird data for 203-2025
+#read in ncos bird data for 2023-2025
 ncos_aquatic_birds_sf <- read_csv(file = "data/for_Emilio/aquatic_bird_observations_2023_2025.csv") %>% 
   st_as_sf(coords = c("x", "y")) %>% 
   #assign WGS84 (not projected)
@@ -12,7 +12,11 @@ ncos_aquatic_birds_sf <- read_csv(file = "data/for_Emilio/aquatic_bird_observati
 st_crs(ncos_aquatic_birds_sf)
 
 # view bird data
-mapview(ncos_aquatic_birds_sf)
+mapview(ncos_aquatic_birds_sf,
+        col.regions = "red"
+)
+
+# Vernal Pools ----
 
 #check coordinate reference system of VernalPools polygon
 st_crs(VernalPools)
@@ -27,6 +31,7 @@ vernal_pools_sf <- VernalPools %>%
 # see documentation on subsetting using brackets: https://r-spatial.github.io/sf/articles/sf4.html#subsetting-feature-sets
 
 vp_birds <- ncos_aquatic_birds_sf[vernal_pools_sf,]
+vp_birds
 
 # view on map
 mapview(vp_birds, map.type = "Esri.WorldImagery")
@@ -36,14 +41,84 @@ vp_bird_summary <- vp_birds %>%
   group_by(species) %>% 
   summarize(total_count = sum(count))
 
-# East and West branches, with incoming southern branch
+# Entire Slough: East and West branches, with incoming Southern branch ----
 
 st_crs(E_W_Main_Zones)
 e_w_s_zones_sf <- E_W_Main_Zones |> 
   st_transform(crs = 4326)
 
+# view
 ews_birds <- ncos_aquatic_birds_sf[e_w_s_zones_sf,]
 mapview(ews_birds, map.type = "Esri.WorldImagery")
 
-# TODO summarize counts
+# summarize
 
+ews_bird_summary <- ews_birds |> 
+  group_by(species) |> 
+  summarize(total_count = sum(count))
+
+# Phelps Creek junction ---- 
+
+st_crs(Phelps)
+phelps_sf <- Phelps |> 
+  st_transform(crs = 4326)
+
+# view
+phelps_birds <- ncos_aquatic_birds_sf[phelps_sf,]
+mapview(phelps_birds, map.type = "Esri.WorldImagery")
+
+# summarize
+
+phelps_bird_summary <- phelps_birds |> 
+  group_by(species) |> 
+  summarize(total_count = sum(count))
+
+# Lower Dev. Slough ---- 
+
+st_crs(Lower_Slough)
+lower_slough_sf <- Lower_Slough |> 
+  st_transform(crs = 4326)
+
+# view
+lower_slough_birds <- ncos_aquatic_birds_sf[lower_slough_sf,]
+mapview(lower_slough_birds, map.type = "Esri.WorldImagery")
+
+# summarize
+
+lower_slough_bird_summary <- lower_slough_birds |> 
+  group_by(species) |> 
+  summarize(total_count = sum(count))
+
+# NCOS ponds ---- 
+
+st_crs(Ponds)
+ponds_sf <- Ponds |> 
+  st_transform(crs = 4326)
+
+# view
+ponds_birds <- ncos_aquatic_birds_sf[ponds_sf,]
+mapview(ponds_birds, map.type = "Esri.WorldImagery")
+
+# summarize
+
+ponds_bird_summary <- ponds_birds |> 
+  group_by(species) |> 
+  summarize(total_count = sum(count))
+
+# East and West Branches only ---- 
+
+st_crs(Branches_Main_Zones)
+e_w_branches_sf <- Branches_Main_Zones |> 
+  st_transform(crs = 4326)
+
+# view
+e_w_branches_birds <- ncos_aquatic_birds_sf[e_w_branches_sf,]
+mapview(e_w_branches_birds, map.type = "Esri.WorldImagery")
+
+# summarize
+
+e_w_branches_bird_summary <- e_w_branches_birds |> 
+  group_by(species) |> 
+  summarize(total_count = sum(count))
+
+ncos_aquatic_birds_sf
