2d6c3d594e1aa39339ad59bb0faf2d1962ccd4f7
beojemann <beojemann@ucsb.edu>
2026-05-01 09:49:13 -0700

    Finished debugging abundance code and creating updated figures + Ideas for next steps.

--- code/Ojemann_Emilio/02_create_networks.R
+++ code/Ojemann_Emilio/02_create_networks.R
@@ -1,12 +1,8 @@
 # source previous script
 
-
 source("code/Ojemann_Emilio/01_read_filter_bird_data.R")
 
-# start creading network for vernal pool bird assemblage
-
 # load packages ----
-
 library(readxl)
 library(tidyverse)
 
@@ -81,16 +77,11 @@ adj_matrix <- as_adjacency_matrix(trophic_network, sparse=FALSE)
 
 # invert abundances are calculated in the "focal_taxa_abundances_by_habitat.R" script and saved to file as invert_abundances.csv
 
-# load in intert abundances ----
 
-invert_abundances <- read_csv("data/for_Emilio/invert_abundances.csv")
 
-invert_abun_vp <- invert_abundances %>% 
-  filter(zone == "vernal pool") %>% 
-  filter(ln_count > 0)
 
-# TODO- subset the abundances similarly for other zones
-
+#COLORS ----
+# Assign colors by prey type
 
 ### vernal pools ----
 
@@ -141,19 +132,8 @@ link_colors_vp[, colnames(matrix_subset_vp) == "Ceratopogonidae"][
   matrix_subset_vp[, colnames(matrix_subset_vp) == "Ceratopogonidae"] > 0] <- "#e76f51"
 
 
-
 # # add arguments for abundances and colors: link_color = link_colors1, 
 # higher_abundances = invert_abundances_vector, lower_abundances = bird_invert_abundances_vector, 
-
-# open png graphics
-png(file = "figures/Ojemann_Emilio/invert_network_vp_color.png", width = 600, height = 900, units = "px", res = 100)
-
-
-# plotting command
-plotweb(web = matrix_subset_vp, text_size =0.8, horizontal = TRUE, link_color = link_colors_vp)
-
-# close png
-dev.off()
 
 
 ###  phelps junction ----
@@ -204,23 +184,11 @@ link_colors_phelps[, colnames(matrix_subset_phelps) == "Ceratopogonidae"][
   matrix_subset_phelps[, colnames(matrix_subset_phelps) == "Ceratopogonidae"] > 0] <- "#e76f51"
 
 
-# # add arguments for abundances and colors: link_color = link_colors1, 
-# higher_abundances = invert_abundances_vector, lower_abundances = bird_invert_abundances_vector, 
-
-# open png graphics
-png(file = "figures/Ojemann_Emilio/invert_network_phelps_color.png", width = 6, height = 8, units = "in", res = 200)
-
-# plot
-plotweb(web = matrix_subset_phelps, text_size =1, horizontal = TRUE, link_color = link_colors_phelps)
-
-# close png
-dev.off()
-
-###  NCOS ponds ----
+###  ncos ponds ----
 
 matrix_subset_ponds <- adj_matrix[ponds_birds_invertivorous$species, inverts]
 
-# color matrix for phelps
+# color matrix for NCOS
 link_colors_ponds <- matrix("gray80",
                              nrow = nrow(matrix_subset_ponds),
                              ncol = ncol(matrix_subset_ponds))
@@ -265,35 +233,14 @@ link_colors_ponds[, colnames(matrix_subset_ponds) == "Ceratopogonidae"][
   matrix_subset_ponds[, colnames(matrix_subset_ponds) == "Ceratopogonidae"] > 0] <- "#e76f51"
 
 
-# # add arguments for abundances and colors: link_color = link_colors1, 
-# higher_abundances = invert_abundances_vector, lower_abundances = bird_invert_abundances_vector, 
 
-# open png graphics
-png(file = "figures/Ojemann_Emilio/invert_network_ponds_color.png", width = 6, height = 8, units = "in", res = 200)
-
-# plot
-plotweb(web = matrix_subset_ponds, text_size =1, horizontal = TRUE, link_color = link_colors_ponds)
-
-# close png
-dev.off()
-
-
-### EWS (overall Dev. Slough)  ----
+### ews (main slough)  ----------
 
 matrix_subset_ews <- adj_matrix[ews_birds_invertivorous$species, inverts] 
 
-# filtering abundance data to ews
-
-invert_abun_ews <- invert_abundances |>  
-  filter(zone == "main slough") |>  
-  filter(ln_count > 0)
-
-invert_abun_ews_vector <- invert_abun_ews
 
 
-
-
-# color matrix for phelps
+# color matrix for EWS
 link_colors_ews <- matrix("gray80",
                             nrow = nrow(matrix_subset_ews),
                             ncol = ncol(matrix_subset_ews))
@@ -338,52 +285,203 @@ link_colors_ews[, colnames(matrix_subset_ews) == "Ceratopogonidae"][
   matrix_subset_ews[, colnames(matrix_subset_ews) == "Ceratopogonidae"] > 0] <- "#e76f51"
 
 
-# # add arguments for abundances and colors: link_color = link_colors1, 
-# higher_abundances = invert_abundances_vector, lower_abundances = bird_invert_abundances_vector, 
 
-glimpse(invert_abundances)
+#ABUNDANCES ---- 
 
-# open png graphics
-png(file = "figures/Ojemann_Emilio/invert_network_ews_color.png", width = 6, height = 8, units = "in", res = 200)
+### #load in invert abundances 
 
-# plot
-plotweb(web = matrix_subset_ews, text_size =1, horizontal = TRUE, link_color = link_colors_ews)
+invert_abundances <- read_csv("data/for_Emilio/invert_abundances.csv")
 
-# close png
+# keep relevant columns
+# using log count because raw counts vary too much
+
+log_invert_abundances <- invert_abundances[, c("invert_taxon", "ln_count", "zone")]
+
+# edit Chironimidae to Chironomidae (the i to o)
+
+log_invert_abundances$invert_taxon[
+  log_invert_abundances$invert_taxon == "Chironimidae"
+] <- "Chironomidae"
+
+# # # GOAL: For each zone: # # #
+# building two vectors, bird and invert abun vectors
+
+#### ews (main slough) -----------
+
+## bird abundance vector ##
+
+# add log count column to bird summary 
+bird_abun_ews <- ews_birds_invertivorous |> 
+  mutate(ln_count = log(total_count + 1)) #prevent log(0)
+
+# pull out numbers as a vector
+bird_abun_ews_vector <- bird_abun_ews$ln_count
+# add species name to each number so R knows which counts are to which birds
+names(bird_abun_ews_vector) <- bird_abun_ews$species
+
+## invert abundance ## 
+
+# filter invert abundance table to only ews/main slough
+invert_abun_ews_log <- log_invert_abundances |> 
+  filter(zone == "main slough")
+
+# pull out numbers as a vector
+invert_abun_ews_vector <- invert_abun_ews_log$ln_count
+# add names to each number so R knows which counts are to which birds
+names(invert_abun_ews_vector) <- invert_abun_ews_log$invert_taxon
+
+## matching vectors to the matrix using names ##
+
+invert_abun_ews_vector <- invert_abun_ews_vector[colnames(matrix_subset_ews)]
+bird_abun_ews_vector <- bird_abun_ews_vector[rownames(matrix_subset_ews)]
+
+#### vernal pools ----
+
+## bird abundance vector ##
+bird_abun_vp <- vp_birds_invertivorous |>
+  mutate(ln_count = log(total_count + 1))
+
+bird_abun_vp_vector <- bird_abun_vp$ln_count
+names(bird_abun_vp_vector) <- bird_abun_vp$species
+
+## invert abundance vector ##
+invert_abun_vp_log <- log_invert_abundances |>
+  filter(zone == "vernal pool")
+
+invert_abun_vp_vector <- invert_abun_vp_log$ln_count
+names(invert_abun_vp_vector) <- invert_abun_vp_log$invert_taxon
+
+## matching vectors to match the matrix ##
+invert_abun_vp_vector <- invert_abun_vp_vector[colnames(matrix_subset_vp)]
+bird_abun_vp_vector   <- bird_abun_vp_vector[rownames(matrix_subset_vp)]
+
+
+#### phelps junction ----
+
+## - bird abundance vector - ##
+bird_abun_phelps <- phelps_birds_invertivorous |>
+  mutate(ln_count = log(total_count + 1))
+
+bird_abun_phelps_vector <- bird_abun_phelps$ln_count
+names(bird_abun_phelps_vector) <- bird_abun_phelps$species
+
+## - invert abundance vector - ##
+# note: "NPB" = phelps zone for log_invert_abundance 
+invert_abun_phelps_log <- log_invert_abundances |>
+  filter(zone == "NPB")
+
+invert_abun_phelps_vector <- invert_abun_phelps_log$ln_count
+names(invert_abun_phelps_vector) <- invert_abun_phelps_log$invert_taxon
+
+## - reorder both vectors to match the matrix - ##
+invert_abun_phelps_vector <- invert_abun_phelps_vector[colnames(matrix_subset_phelps)]
+bird_abun_phelps_vector   <- bird_abun_phelps_vector[rownames(matrix_subset_phelps)]
+
+
+#### ncos ponds ---------
+
+#---bird abundance---#
+bird_abun_ponds <- ponds_birds_invertivorous |>
+  mutate(ln_count = log(total_count + 1))
+
+bird_abun_ponds_vector <- bird_abun_ponds$ln_count
+names(bird_abun_ponds_vector) <- bird_abun_ponds$species
+
+#---invert abundance vector---#
+invert_abun_ponds_log <- log_invert_abundances |>
+  filter(zone == "pond")
+
+invert_abun_ponds_vector <- invert_abun_ponds_log$ln_count
+names(invert_abun_ponds_vector) <- invert_abun_ponds_log$invert_taxon
+
+##---get vectors to match---##
+invert_abun_ponds_vector <- invert_abun_ponds_vector[colnames(matrix_subset_ponds)]
+bird_abun_ponds_vector   <- bird_abun_ponds_vector[rownames(matrix_subset_ponds)]
+
+
+
+
+#Saving as PNG ----
+###---### 
+# NOTE: 
+# all figures with "final" in the file name have abundance AND color
+# figures with "color" have only color
+# 
+# ews_color was inadvertently saved in the process of debugging the abundance code, thus only invert_network_ews and invert_network_ews_final_color are visible
+
+##### ---- ews ----
+png(file = "figures/Ojemann_Emilio/invert_network_ews_final_color.png", width = 6, height = 8, units = "in", res = 200)
+plotweb(web = matrix_subset_ews, text_size = 0.8, horizontal = TRUE, curved_links = TRUE,
+        link_color = link_colors_ews,
+        higher_abundances = invert_abun_ews_vector,
+        lower_abundances = bird_abun_ews_vector)
 dev.off()
 
 
-#TODO- filter invertebrate nodes to taxa actually present at sampling sites
-#continue filling out trophic links data
-#create draft network viz based on existing data 
-#
-# Abundance data needed to create a weighted matrix
-# link width = size of interaction
-# Utilize plotweb() to get weight
+##### ---- vernal pools -----
+png(file = "figures/Ojemann_Emilio/invert_network_vp_final_color.png", width = 6, height = 8, units = "in", res = 200)
+plotweb(web = matrix_subset_vp, text_size = 0.6, horizontal = TRUE, curved_links = TRUE,
+        link_color = link_colors_vp,
+        higher_abundances = invert_abun_vp_vector,
+        lower_abundances = bird_abun_vp_vector)
+dev.off()
+
+#### ---- phelps ----
+png(file = "figures/Ojemann_Emilio/invert_network_phelps_final_color.png", width = 6, height = 8, units = "in", res = 200)
+plotweb(web = matrix_subset_phelps, text_size = 0.6, horizontal = TRUE, curved_links = TRUE,
+        link_color = link_colors_phelps,
+        higher_abundances = invert_abun_phelps_vector,
+        lower_abundances = bird_abun_phelps_vector)
+dev.off()
+
+#### ---- ponds----
+png(file = "figures/Ojemann_Emilio/invert_network_ponds_final_color.png", width = 6, height = 8, units = "in", res = 200)
+plotweb(web = matrix_subset_ponds, text_size = 0.6, horizontal = TRUE, curved_links = TRUE,
+        link_color = link_colors_ponds,
+        higher_abundances = invert_abun_ponds_vector,
+        lower_abundances = bird_abun_ponds_vector)
+dev.off()
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ----
+
+
+#   ---- TO-DO ---- 
+
+# filter invertebrate nodes to taxa actually present at sampling sites
+# --> would then need to rewrite colors
+#   --> Ceratopogonidae never appears at NCOS
+#   --> Ephydridae only appears in vp and ponds
 # 
-# Looking at:
-# Habitats supporting the most involved networks
-# Vernal pools vs. ponds vs. slough
-# What taxa are seen everywhere, what taxa are not?
+# continue filling out trophic links data (????)
+
+# analysis  ----
+
+# CURRENTLY DOING READING INTO THIS:
+help("networklevel")
+
+ews_network_metrics <- networklevel(matrix_subset_ews)
+
+# relevant "networklevel" values:
+
+# connectance = standardized number of species combinations or total numner of unique combinations of species
+ 
+# links per species
+# nestedness = the disorder of the network, is it ordered or does it deviate from order? Lower temperature = higher nestedness = more ecosystem stability b/c specialists survive by sharing the same resources as generalists, protecting the network fromc secondary extinctions.
+# NODF = corrected nestedness. Higher values = higher nestedness
 # 
-# Bigger next steps:
-### Seasonal breakdown
-### Revisit time period availability
-### What new data do we have to add?
-### Can we do any prediction/simulation with the data?
-### Poster/visualizing -- story map? interactive map? what figures best display the results we have
-
-
-# COLORS ----
-# Assign colors by prey type
-
-inverts
-
-unique(invert_abundances$invert_taxon)
-
-# obtaining invert abundances
-# filter to actual existing taxa
 
 
 
+
+###--- next steps? if any feasible?: ---###
+# Seasonal breakdown
+#Revisit time period availability
+# What new data do we have to add?
+# Can we do any prediction/simulation with the data?
+
+
+###--- Poster/visualizing ---###
+# -- story map? 
+# interactive map? 
+# what figures best display the results we have
 
